4c2bcb507c9c6be63f8aed93540e65d433445a42
github-action-benchmark <github@users.noreply.github.com>
2022-10-28 18:28:41 +0000

    add Benchmark (go) benchmark result for d10b249c83b2a7262788d6a8df8f749dcf3ab461

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1666981167073,
+  "lastUpdate": 1666981721012,
   "repoUrl": "https://github.com/gsdenys/meparser",
   "entries": {
     "Benchmark": [
@@ -34,6 +34,42 @@ window.BENCHMARK_DATA = {
           {
             "name": "Benchmark_E121_Run",
             "value": 0.0000946,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "gsdenys@gmail.com",
+            "name": "Denys G. Santos",
+            "username": "gsdenys"
+          },
+          "committer": {
+            "email": "gsdenys@gmail.com",
+            "name": "Denys G. Santos",
+            "username": "gsdenys"
+          },
+          "distinct": true,
+          "id": "d10b249c83b2a7262788d6a8df8f749dcf3ab461",
+          "message": "create benchmark",
+          "timestamp": "2022-10-28T14:17:27-04:00",
+          "tree_id": "3107ac8112f6f8c0e0acc50bce7e77d58f339001",
+          "url": "https://github.com/gsdenys/meparser/commit/d10b249c83b2a7262788d6a8df8f749dcf3ab461"
+        },
+        "date": 1666981720521,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "Benchmark_E11000_Run",
+            "value": 0.0000494,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n2 procs"
+          },
+          {
+            "name": "Benchmark_E121_Run",
+            "value": 0.0000937,
             "unit": "ns/op",
             "extra": "1000000000 times\n2 procs"
           }
